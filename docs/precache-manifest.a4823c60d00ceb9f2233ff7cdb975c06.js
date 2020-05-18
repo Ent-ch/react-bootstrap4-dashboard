9c4421b78dc803725bca81500d947f61727108aa
Vladimir Saakyan <v.saakyan.allegra@gmail.com>
2020-05-18 15:21:16 +0300

    added build

---
+++ docs/precache-manifest.a4823c60d00ceb9f2233ff7cdb975c06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff0147cb99ef8792f3bf8dc295a8adb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffa3074522667975d035",
+    "url": "/static/css/2.1bfd5170.chunk.css"
+  },
+  {
+    "revision": "58df4a897f6f6b90593b",
+    "url": "/static/css/main.32727de0.chunk.css"
+  },
+  {
+    "revision": "ffa3074522667975d035",
+    "url": "/static/js/2.898ff07a.chunk.js"
+  },
+  {
+    "revision": "58df4a897f6f6b90593b",
+    "url": "/static/js/main.4caaf892.chunk.js"
+  },
+  {
+    "revision": "0137ada024c9dba663b3",
+    "url": "/static/js/runtime~main.3a6bdb62.js"
+  },
+  {
+    "revision": "9323b354dd62e1475d83dd60158aa1a4",
+    "url": "/static/media/background-cloud.9323b354.jpg"
+  }
+]);
